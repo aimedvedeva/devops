fcebbef58e3ff2177d925c58a6e581c0f12cda83
Aleksandra Medvedeva <62102269+aimedvedeva@users.noreply.github.com>
2024-06-05 20:47:57 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+  "github.com/prometheus/client_golang/prometheus"
+  "github.com/prometheus/client_golang/prometheus/promauto"
+  "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type Simple struct {
+	Name        string
+	Description string
+	Url         string
+}
+
+var counter = promauto.NewCounter(prometheus.CounterOpts{
+  Name: "api_calls_total"
+  Help: "The total number of processed events"
+})
+
+func SimpleFactory(host string) Simple {
+	return Simple{"Hello", "World", host}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	simple := SimpleFactory(r.Host)
+
+	jsonOutput, _ := json.Marshal(simple)
+
+  counter.Inc()
+
+	fmt.Fprintln(w, string(jsonOutput))
+}
+
+func main() {
+	fmt.Println("Server started on port 4444")
+	http.HandleFunc("/api", handler)
+  http.Handle("/metrics", promhttp.Handler())
+  fmt.Println("Monitoring endpoint added")
+  log.Fatal(http.ListenAndServe(":4444", nil))
+}
